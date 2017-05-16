78d471bcced42b3319ac8b04d3eea1642e282b97
Alexander Ismaier <alex.ismaier@freenet.de>
2017-05-16 21:16:45 +0200

    whoops

---
+++ src/pages/enterQuestion/enterQuestion.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-enterQuestion',
+  templateUrl: 'enterQuestion.html'
+})
+export class EnterQuestionPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
