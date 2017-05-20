44b4f28c677cccce40393f9637ba76a44c8e31c6
Alexander Ismaier <alex.ismaier@freenet.de>
2017-05-20 16:20:58 +0200

    delicious toast

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, ToastController} from 'ionic-angular';
 
 import { TabsPage } from '../tabs/tabs';
 import {SignUpPage} from '../signUp/signUp';
@@ -17,19 +17,24 @@ export class LoginPage {
   email = "";
   password = "";
 
-  constructor(public navCtrl: NavController, public storage: Storage) {
+  constructor(public navCtrl: NavController, public storage: Storage, public toastCtrl: ToastController) {
   }
 
   login() {
     if(true){ //TODO check if user account exists, maybe special message if not activated yet
       this.storage.set('localUserEmail', this.email);
+      this.storage.set('localUserPassword', this.password);
       this.navCtrl.push(this.tabsView);
       // this.storage.get('localUserEmail').then((val) => {
       //   console.log('Email is: ', val);
       // });
     }
     else{
-      //message: wrong input
+      // let toast = this.toastCtrl.create({
+      //   message: 'Anmeldung fehlgeschlagen. E-Mail oder Passwort sind ungültig.',
+      //   duration: 3000
+      // });
+      // toast.present();
     }
   }
 
