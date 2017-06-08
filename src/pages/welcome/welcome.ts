4d3c7f28dadef3a5e2ad58029af45dddf9848f44
Alexander Ismaier <alex.ismaier@freenet.de>
2017-06-08 12:23:28 +0200

    added files

---
+++ src/pages/welcome/welcome.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {NavController, ToastController} from 'ionic-angular';
+
+import {SignUpPage} from '../signUp/signUp';
+import {LoginPage} from '../login/login';
+import { Storage } from '@ionic/storage';
+import {TranslateService} from "@ngx-translate/core";
+
+@Component({
+  selector: 'page-welcome',
+  templateUrl: 'welcome.html'
+})
+export class WelcomePage {
+
+  signUpView = SignUpPage;
+  loginView = LoginPage;
+
+  constructor(public navCtrl: NavController, public storage: Storage, public toastCtrl: ToastController, translate: TranslateService) {
+
+  }
+}
