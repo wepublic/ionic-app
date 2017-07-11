b47dbc7ad072041640f5d1b5121ddf1cfd14d35d
Manuel Stahl <thymythos@googlemail.com>
2017-07-11 17:50:38 +0200

    Add proxy definition to ionic configuration
    The proxy allows to access the backend even when no
    Cross-Origin Resource Sharing is defined.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,1 +1,2 @@
-export let API_ENDPOINT = 'https://boiling-spire-20724.herokuapp.com';
+export let API_ENDPOINT = 'http://localhost:8100/api';
+//export let API_ENDPOINT = 'https://boiling-spire-20724.herokuapp.com';
