b7a70d657eaff459752958cce707c6aa7890b074
Manuel Stahl <thymythos@googlemail.com>
2017-08-04 15:18:46 +0200

    Do not copy assets explicitly
    Not needed in Ionic 2.

--- config/copy.config.js
+++ config/copy.config.js
@@ -2,10 +2,6 @@
 // provide a name for an entry, it can be anything such as 'copyAssets' or 'copyFonts'
 // then provide an object with a `src` array of globs and a `dest` string
 module.exports = {
-  copyAssets: {
-    src: ['{{SRC}}/assets/**/*'],
-    dest: '{{WWW}}/assets'
-  },
   copyIndexContent: {
     src: ['{{SRC}}/index.html', '{{SRC}}/manifest.json', '{{SRC}}/service-worker.js'],
     dest: '{{WWW}}'
