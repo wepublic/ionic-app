ed2bf6b05f4452fd145ce5d3441246185f08b0d2
Manuel Stahl <thymythos@googlemail.com>
2017-08-11 09:17:02 +0200

    Release 0.0.5

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,2 +1,2 @@
-export let API_ENDPOINT = 'http://localhost:8100/api';
-//export let API_ENDPOINT = 'https://api.wepublic.me/v1';
+//export let API_ENDPOINT = 'http://localhost:8100/api';
+export let API_ENDPOINT = 'https://api.wepublic.me/v1';
