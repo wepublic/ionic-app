78c4b74a0704c6e25dd931d3a4d7028521bd396a
Manuel Stahl <thymythos@googlemail.com>
2017-08-14 13:42:57 +0200

    Add onboarding slides

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { Storage } from '@ionic/storage';
 import {RandomQuestionsPage} from "../randomQuestions/randomQuestions";
-import {NavController} from "ionic-angular";
+import {NavController, Tabs} from "ionic-angular";
 import {SearchQuestionsPage} from "../searchQuestions/searchQuestions";
 import {MainMenuPage} from "../mainMenu/mainMenu";
 
@@ -9,12 +9,23 @@ import {MainMenuPage} from "../mainMenu/mainMenu";
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
+  @ViewChild(Tabs) tabs: Tabs;
+  @ViewChild('onboarding') onboarding;
 
   tab1Root = RandomQuestionsPage;
   tab2Root = MainMenuPage;
   tab3Root = SearchQuestionsPage;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public storage: Storage, public navCtrl: NavController) {
+  }
 
+  ionViewWillEnter() {
+    this.storage.get('showedOnboarding').then(val => { if (val) this.hideOnboarding(); });
+  }
+
+  hideOnboarding() {
+    this.tabs.setElementClass('blurred', false);
+    this.onboarding.nativeElement.style.display = 'none';
+    this.storage.set('showedOnboarding', true);
   }
 }
