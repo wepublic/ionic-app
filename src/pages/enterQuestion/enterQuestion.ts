ab6481f89cb4ae4ed1a96e1ea2154400220b9631
Manuel Stahl <thymythos@googlemail.com>
2017-08-23 15:15:27 +0200

    Limit amount of #tags in enter question page
    Fixes #43.

--- src/pages/enterQuestion/enterQuestion.ts
+++ src/pages/enterQuestion/enterQuestion.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { TranslateService } from "@ngx-translate/core";
 import { TagsHelper } from "../../utils/TagsHelper";
 import { QuestionServiceProvider } from "../../providers/question-service/question-service";
@@ -11,32 +11,41 @@ import { MainMenuPage } from "../mainMenu/mainMenu";
 })
 export class EnterQuestionPage {
 
+  public maxTags = 3;
   public maxTextLength = 250;
   public tags = [];
-  public selectedTags;
+  public selectedTags: number[] = [];
   public questionText: string = "";
 
-  private selectOptions = {
-    title: ""
-  };
-
-  constructor(private navCtrl: NavController, translate: TranslateService,
+  constructor(private navCtrl: NavController, private toastCtrl: ToastController, private translate: TranslateService,
               private tagsHelper: TagsHelper, private questionService: QuestionServiceProvider) {
     this.tags = this.tagsHelper.getAllTagObjectsSorted();
-    translate.get('ENTERQUESTION.TAG_QUESTION', {value: 'world'}).subscribe((res: string) => this.selectOptions.title = res);
   }
 
+  showErrorToast(text: string) {
+    this.translate.get(text, {value: 'world'})
+    .subscribe(text => this.toastCtrl.create({ message: text, duration: 3000 }).present());
+  }
+    
+  onSelectTags() {
+    if (this.selectedTags.length > 3) this.selectedTags.length = 3;
+  }
+    
   publishQuestion() {
     console.log('publish question');
     console.log(this.questionText);
     console.log(this.selectedTags);
-    if (this.questionText !== undefined && this.selectedTags !== undefined) {
-      if (this.questionText.length > 0 && this.selectedTags.length > 0) {
-        this.questionService.publishQuestion(this.questionText, this.selectedTags)
-        .subscribe(data => { console.log(data); });
-        this.navCtrl.setRoot(MainMenuPage);
-      }
-    }
+    if (this.selectedTags === undefined || this.selectedTags.length == 0) {
+      this.showErrorToast('ENTERQUESTION.ERROR_NO_TAGS');
+      return;
+    }        
+    if (this.questionText === undefined || this.questionText.length < 10) {
+      this.showErrorToast('ENTERQUESTION.ERROR_SHORT_TEXT');
+      return;
+    } 
+    this.questionService.publishQuestion(this.questionText, this.selectedTags)
+    .subscribe(data => { console.log(data); });
+    this.navCtrl.setRoot(MainMenuPage);
   }
 
 }
